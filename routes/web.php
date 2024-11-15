378f7229aa8f470ec4e23df09bf59cc03c060b30
Marumaru0000 <k-maruyama-5x3@eagle.sophia.ac.jp>
2024-11-15 10:07:17 +0900

    Initial commit with self-ordering-develop setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+#use Illuminate\Support\Facades\Route;
+
+#Route::get('/', function () {
+    #return view('welcome');
+#});
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// セルフオーダーシステムのトップページとしてQRコードの表示ページを設定
+Route::view('/', 'ordering::help');
+Route::get('/test-microcms', function () {
+    $client = new \GuzzleHttp\Client();
+    $response = $client->request('GET', env('ORDERING_MICROCMS_ENDPOINT'), [
+        'headers' => [
+            'X-API-KEY' => env('ORDERING_MICROCMS_API_KEY'),
+        ],
+    ]);
+
+    return json_decode($response->getBody(), true);
 });
